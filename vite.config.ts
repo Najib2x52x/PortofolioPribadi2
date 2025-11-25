82effb0644a1247c7fb7656df7b30e075fbf833f
Najib2x52x <1aghismuhammad@gmail.com>
2025-11-25 15:12:25 +0700

    penyesuaian dengan vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+        input: {
+            // Halaman utama
+            main: resolve(__dirname, 'index.html'),
+            // Halaman detail (PENTING: ini agar file ini ikut di-build ke Vercel)
+            detail: resolve(__dirname, 'detail-project.html'),
+        },
+        },
+    },
 })
